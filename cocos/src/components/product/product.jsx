2d4b5493f280167832d43f539fecd65c1a10a861
zhaoyan <13811878882@126.com>
2019-05-27 15:46:31 +0800

    1

--- cocos/src/components/product/product.jsx
+++ cocos/src/components/product/product.jsx
@@ -17,27 +17,23 @@ export default class Product extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            gameList:[
-                {img:fruit,url:'http://game.cocos-bcx.io/fruit/',til:'gname1',auther:'gauther1',gex:'gex1'},
-                {img:luck,url:'http://tyche-bcx.oriongaming.club/',til:'gname2',auther:'gauther2',gex:'gex2'},
-                {img:shotting,url:'http://ccshooter.oriongaming.club/',til:'gname3',auther:'gauther3',gex:'gex3'},
+            gameList: [
+                { img: fruit, url: 'http://game.cocos-bcx.io/fruit/', til: 'gname1', auther: 'gauther1', gex: 'gex1' },
+                { img: luck, url: 'http://tyche-bcx.oriongaming.club/', til: 'gname2', auther: 'gauther2', gex: 'gex2' },
+                { img: shotting, url: 'http://ccshooter.oriongaming.club/', til: 'gname3', auther: 'gauther3', gex: 'gex3' },
             ],
-            toolList:[
-                {img:tool1,url:'https://explorer.cocosbcx.io/',til:'tname1',auther:'tauther1',gex:'tex1'},
-                {img:tool2,url:'http://www.easywallet.pro/',til:'tname2',auther:'tauther2',gex:'tex2'},
-                {img:tool3,url:'http://gpe.famegame.com.cn/',til:'tname3',auther:'tauther3',gex:'tex3'},
-                {img:tool4,url:'http://gpe.famegame.com.cn/',til:'tname4',auther:'tauther4',gex:'tex4'},
-                {img:tool5,url:'http://gpe.famegame.com.cn/',til:'tname5',auther:'tauther5',gex:'tex5'},
-                {img:tool6,url:'http://gpe.famegame.com.cn/',til:'tname6',auther:'tauther6',gex:'tex6'},
-                {img:tool7,url:'http://gpe.famegame.com.cn/',til:'tname7',auther:'tauther7',gex:'tex7'},
+            toolList: [
+                { img: tool1, url: 'https://explorer.cocosbcx.io/', til: 'tname1', auther: 'tauther1', gex: 'tex1' },
+                { img: tool2, url: 'http://www.easywallet.pro/', til: 'tname2', auther: 'tauther2', gex: 'tex2' },
+                { img: tool3, url: 'http://gpe.famegame.com.cn/', til: 'tname3', auther: 'tauther3', gex: 'tex3' },
             ],
         }
     }
-    componentDidMount(){
-        this.devbtn.onmouseenter=()=>{
+    componentDidMount() {
+        this.devbtn.onmouseenter = () => {
             this.devbtn.classList.add('bounceIn');
         }
-        this.devplan.onmouseleave=()=>{
+        this.devplan.onmouseleave = () => {
             this.devbtn.classList.remove('bounceIn');
         }
     }
@@ -49,12 +45,12 @@ export default class Product extends Component {
                 <div className="banner_s_box" style={{ background: "url('https://jdi.cocosbcx.net/image/cocosbcx/product_banner.jpg') no-repeat center" }}>
                     <Nav></Nav>
                 </div>
-                <div className='dev_plan' ref={(x)=>{this.devplan=x}}>
-                    <a  href={lang==='zh'?"https://mp.weixin.qq.com/s/EuM8mGfXh3QpwKLN7i9ITA":'https://medium.com/@CocosBCX/cocos-bcx-alpha-testing-now-open-to-developers-globally-908880f67de7'} className='ljdev animated' target='_blank' rel="noopener noreferrer">
-                        <span ref={(x)=>{this.devbtn=x}} className='animated' >
-                     {t.ljdev}
+                <div className='dev_plan' ref={(x) => { this.devplan = x }}>
+                    <a href={lang === 'zh' ? "https://mp.weixin.qq.com/s/EuM8mGfXh3QpwKLN7i9ITA" : 'https://medium.com/@CocosBCX/cocos-bcx-alpha-testing-now-open-to-developers-globally-908880f67de7'} className='ljdev animated' target='_blank' rel="noopener noreferrer">
+                        <span ref={(x) => { this.devbtn = x }} className='animated' >
+                            {t.ljdev}
                         </span>
-                      
+
                     </a>
                 </div>
                 <div className='dev_list_box'>
@@ -67,19 +63,19 @@ export default class Product extends Component {
                             <div className="news_til_mask"></div>
                         </div>
                         <div className='dev_game_box'>
-                        {this.state.gameList.map((item,index)=>{
-                            return  <a href={item.url} target='_blank' rel="noopener noreferrer" className='dev_game_left lt' key={index}>
-                            <div className='img_box lt'>
-                                <img src={item.img} alt="" />
-                            </div>
-                            <div className='dev_text_box lt'>
-                                <h4>{t[item.til]}</h4>
-                                <div className='wordroom'>{t[item.auther]}</div>
-                                <p>{t[item.gex]}</p>
-                            </div>
-                        </a>
-                        })}
-                        
+                            {this.state.gameList.map((item, index) => {
+                                return <a href={item.url} target='_blank' rel="noopener noreferrer" className='dev_game_left lt' key={index}>
+                                    <div className='img_box lt'>
+                                        <img src={item.img} alt="" />
+                                    </div>
+                                    <div className='dev_text_box lt'>
+                                        <h4>{t[item.til]}</h4>
+                                        <div className='wordroom'>{t[item.auther]}</div>
+                                        <p>{t[item.gex]}</p>
+                                    </div>
+                                </a>
+                            })}
+
                         </div>
                     </div>
                     <div className='dev_game'>
@@ -91,18 +87,18 @@ export default class Product extends Component {
                             <div className="news_til_mask"></div>
                         </div>
                         <div className='dev_game_box'>
-                        {this.state.toolList.map((item,index)=>{
-                            return  <a href={item.url} target='_blank' rel="noopener noreferrer" className='dev_game_left lt' key={index}>
-                            <div className='img_box lt'>
-                                <img src={item.img} alt="" />
-                            </div>
-                            <div className='dev_text_box lt'>
-                                <h4>{t[item.til]}</h4>
-                                <div className='wordroom'>{t[item.auther]}</div>
-                                <p>{t[item.gex]}</p>
-                            </div>
-                        </a>
-                        })}
+                            {this.state.toolList.map((item, index) => {
+                                return <a href={item.url} target='_blank' rel="noopener noreferrer" className='dev_game_left lt' key={index}>
+                                    <div className='img_box lt'>
+                                        <img src={item.img} alt="" />
+                                    </div>
+                                    <div className='dev_text_box lt'>
+                                        <h4>{t[item.til]}</h4>
+                                        <div className='wordroom'>{t[item.auther]}</div>
+                                        <p>{t[item.gex]}</p>
+                                    </div>
+                                </a>
+                            })}
                         </div>
                     </div>
                 </div>
