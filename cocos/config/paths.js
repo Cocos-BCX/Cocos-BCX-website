d57d5aeecb1cfb4e9cf6f7e2d9877c3d5c55b060
zhaoyan <13811878882@126.com>
2019-07-09 17:35:57 +0800

    1

--- cocos/config/paths.js
+++ cocos/config/paths.js
@@ -34,7 +34,7 @@ const getPublicUrl = appPackageJson =>
 function getServedPath(appPackageJson) {
   const publicUrl = getPublicUrl(appPackageJson);
   const servedUrl =
-    envPublicUrl || (publicUrl ? url.parse(publicUrl).pathname : 'https://jdi.cocosbcx.net/webpc/');
+    envPublicUrl || (publicUrl ? url.parse(publicUrl).pathname : './');
   return ensureSlash(servedUrl, true);
 }
 
