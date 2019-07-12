d7feeacc0a2d6556e6957de390a87e4b4fcb0e58
zhaoyan <13811878882@126.com>
2019-07-12 14:56:56 +0800

    1

--- cocos/src/router/router.jsx
+++ cocos/src/router/router.jsx
@@ -5,8 +5,6 @@ import About from '../components/about/about'
 import Product from '../components/product/product'
 import Footer from '../components/footer/footer'
 import Action from '../components/action/action'
-import Ecologicalnodes from '../components/ecologicalnodes/ecologicalnodes'
-import ExcitationConsultation from '../components/excitationConsultation/excitationConsultation'
 import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
 import './router.css'
 export default class Homepage extends Component {
@@ -14,6 +12,7 @@ export default class Homepage extends Component {
     render() {
         return (
             <div className="router">
+
                 <div className='homepage_main'>
                     <Router >
                         <Switch >
@@ -23,8 +22,6 @@ export default class Homepage extends Component {
                             <Route path="/about" component={About} />
                             <Route path="/product" component={Product} />
                             <Route path="/action" component={Action} />
-                            <Route path="/ecologicalnodes" component={Ecologicalnodes} />
-                            <Route path="/excitationconsultation" component={ExcitationConsultation} />
                             <Route path='/' render={() => (
                                 <Redirect to='/' />
                             )} />
